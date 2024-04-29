23d4b5f0bfb50b7409a41cbff5609b3790c244cb
Prasanna <github@prasansrini.com>
2024-04-29 14:28:59 +0200

    Changed ConstraintLayout as a component.

--- app/src/main/java/com/example/composesample/components/Components.kt
+++ app/src/main/java/com/example/composesample/components/Components.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -48,6 +49,10 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -258,5 +263,44 @@ fun LazyScrollableList() {
 					.padding(24.dp)
 			)
 		}
+	}
+}
+
+@Composable
+fun ConstraintLayoutDemo() {
+	val constraints = ConstraintSet {
+		val greenBox = createRefFor("greenbox")
+		val redBox = createRefFor("redbox")
+		val guideLine = createGuidelineFromTop(0.5f)
+
+		constrain(greenBox) {
+			top.linkTo(guideLine)
+			start.linkTo(parent.start)
+			width = Dimension.value(100.dp)
+			height = Dimension.value(100.dp)
+		}
+
+		constrain(redBox) {
+			top.linkTo(parent.top)
+			start.linkTo(greenBox.end)
+			end.linkTo(parent.end)
+			width = Dimension.value(100.dp)
+			height = Dimension.value(100.dp)
+		}
+		createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+	}
+
+	ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+		Box(
+			modifier = Modifier
+				.background(Color.Green)
+				.layoutId("greenbox")
+		)
+
+		Box(
+			modifier = Modifier
+				.background(Color.Red)
+				.layoutId("redbox")
+		)
 	}
 }
