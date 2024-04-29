6efe8ce53aef58369e882fc3fa3c4feef966deca
Prasanna <github@prasansrini.com>
2024-04-29 11:42:35 +0200

    Modifiers

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.composesample
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -18,20 +24,34 @@ class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Row(
+			Column(
 				modifier = Modifier
-					.background(Color.LightGray)
-					.width(300.dp)
-					.height(300.dp),
-				verticalAlignment = Alignment.CenterVertically,
-				horizontalArrangement = Arrangement.SpaceEvenly
+					.background(Color.Green)
+					.fillMaxHeight(0.5f)
+					.fillMaxWidth()
+					.border(4.dp, Color.Blue)
+					.padding(5.dp)
+					.border(5.dp, Color.Gray)
+					.padding(10.dp)
+					.border(10.dp, Color.Black)
+					.padding(10.dp)
 			) {
-				Text(text = "Hello")
-				Text(text = "World!")
-				Text(text = "Hello")
-				Text(text = "World!")
-				Text(text = "Hello")
-				Text(text = "World!")
+				Text(
+					text = "Hello",
+					modifier = Modifier
+						.border(5.dp, Color.Red)
+						.padding(5.dp)
+						.offset(20.dp, 20.dp)
+						.border(10.dp, Color.Black)
+						.padding(10.dp)
+				)
+				Spacer(modifier = Modifier.height(50.dp))
+				Text(text = "World!", modifier = Modifier.clickable {
+					Toast.makeText(
+						this@MainActivity, "Hello", Toast.LENGTH_SHORT
+					).show()
+					Log.e("MainActivity", "Hello")
+				})
 			}
 		}
 	}
