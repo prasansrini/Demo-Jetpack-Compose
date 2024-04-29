351307caf510cf482a38d641711a47bcdbba44a1
Prasanna <github@prasansrini.com>
2024-04-29 15:28:52 +0200

    Added implementation for ProgressBar.

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.composesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.composesample.components.AnimationStyle
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.composesample.components.CircularProgressBar
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
 		setContent {
-			AnimationStyle()
+			Box(
+				modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+			) {
+				CircularProgressBar(percent = 0.4f, number = 100)
+			}
 		}
 	}
 }
--- app/src/main/java/com/example/composesample/components/Components.kt
+++ app/src/main/java/com/example/composesample/components/Components.kt
@@ -8,11 +8,13 @@ import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -39,6 +41,7 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -48,6 +51,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
@@ -60,6 +65,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
@@ -362,5 +369,52 @@ fun AnimationStyle() {
 		}) {
 			Text(text = "Increase size!")
 		}
+	}
+}
+
+@Composable
+fun CircularProgressBar(
+	percent: Float,
+	number: Int,
+	fontSize: TextUnit = 28.sp,
+	radius: Dp = 50.dp,
+	color: Color = Color.Gray,
+	strokeWidth: Dp = 8.dp,
+	animationDuration: Int = 1000,
+	animationDelay: Int = 0
+) {
+	var animationPlayed by remember {
+		mutableStateOf(false)
+	}
+
+	val currentPercentage = animateFloatAsState(
+		targetValue = if (animationPlayed) percent else 0f, animationSpec = tween(
+			durationMillis = animationDuration, delayMillis = animationDelay
+		), label = ""
+	)
+
+	LaunchedEffect(key1 = true) {
+		animationPlayed = true
+	}
+
+	Box(
+		modifier = Modifier.size(radius * 2f), contentAlignment = Alignment.Center
+	) {
+		Canvas(modifier = Modifier.size(radius * 2f)) {
+			drawArc(
+				color = color,
+				-90f,
+				360 * currentPercentage.value,
+				useCenter = false,
+				style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+			)
+		}
+
+		Text(
+			text = (currentPercentage.value * number).toInt().toString() + "%",
+			color = Color.Black,
+			fontSize = fontSize,
+			fontWeight = FontWeight.Bold
+		)
 	}
 }
