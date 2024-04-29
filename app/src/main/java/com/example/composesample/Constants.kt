d2996662c4ec499a897bf902c74e100a791819ab
Prasanna <github@prasansrini.com>
2024-04-29 12:36:05 +0200

    Added text styling component.

---
+++ app/src/main/java/com/example/composesample/Constants.kt
@@ -0,0 +1,17 @@
+package com.example.composesample
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+object Components {
+	val fontFamily = FontFamily(
+		Font(R.font.lexend_thin, FontWeight.Thin),
+		Font(R.font.lexend_light, FontWeight.Light),
+		Font(R.font.lexend_regular, FontWeight.Normal),
+		Font(R.font.lexend_medium, FontWeight.Medium),
+		Font(R.font.lexend_semibold, FontWeight.SemiBold),
+		Font(R.font.lexend_bold, FontWeight.Bold),
+		Font(R.font.lexend_extrabold, FontWeight.ExtraBold)
+	)
+}
--- app/src/main/java/com/example/composesample/components/Components.kt
+++ app/src/main/java/com/example/composesample/components/Components.kt
@@ -19,7 +19,15 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -60,5 +68,44 @@ fun ImageCard(
 				Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
 			}
 		}
+	}
+}
+
+@Composable
+fun TextStyling(fontFamily: FontFamily) {
+	Box(
+		modifier = Modifier
+			.fillMaxSize()
+			.background(
+				Color(0xFF101010)
+			)
+	) {
+		Text(
+			text = buildAnnotatedString {
+				withStyle(
+					style = SpanStyle(
+						color = Color.Green, fontSize = 50.sp
+					)
+				) {
+					append("J")
+				}
+				append("etpack")
+				withStyle(
+					style = SpanStyle(
+						color = Color.Green, fontSize = 50.sp
+					)
+				) {
+					append("C")
+				}
+				append("ompose")
+			},
+			color = Color.White,
+			fontSize = 30.sp,
+			fontFamily = fontFamily,
+			fontWeight = FontWeight.Bold,
+			fontStyle = FontStyle.Italic,
+			textAlign = TextAlign.Center,
+			textDecoration = TextDecoration.Underline
+		)
 	}
 }
