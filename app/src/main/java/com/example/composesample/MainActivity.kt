0acccbbe4e29123e4853616acdb383abb95fee85
Prasanna <github@prasansrini.com>
2024-04-29 12:56:37 +0200

    Changed implementation for changing color from Components.kt.

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -3,41 +3,14 @@ package com.example.composesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.example.composesample.components.ColorBox
+import com.example.composesample.components.ColorBoxSetContent
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
 		setContent {
-			Column(modifier = Modifier.fillMaxSize()) {
-				val color = remember {
-					mutableStateOf(Color.Yellow)
-				}
-
-				ColorBox(
-					modifier = Modifier
-						.weight(1f)
-						.fillMaxSize()
-				) {
-					color.value = it
-				}
-
-				Box(
-					modifier = Modifier
-						.background(color.value)
-						.weight(1f)
-						.fillMaxSize()
-				)
-			}
+			ColorBoxSetContent()
 		}
 	}
 }
--- app/src/main/java/com/example/composesample/components/Components.kt
+++ app/src/main/java/com/example/composesample/components/Components.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,6 +14,8 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -107,6 +110,30 @@ fun TextStyling(fontFamily: FontFamily) {
 			fontStyle = FontStyle.Italic,
 			textAlign = TextAlign.Center,
 			textDecoration = TextDecoration.Underline
+		)
+	}
+}
+
+@Composable
+fun ColorBoxSetContent() {
+	Column(modifier = Modifier.fillMaxSize()) {
+		val color = remember {
+			mutableStateOf(Color.Yellow)
+		}
+
+		ColorBox(
+			modifier = Modifier
+				.weight(1f)
+				.fillMaxSize()
+		) {
+			color.value = it
+		}
+
+		Box(
+			modifier = Modifier
+				.background(color.value)
+				.weight(1f)
+				.fillMaxSize()
 		)
 	}
 }
